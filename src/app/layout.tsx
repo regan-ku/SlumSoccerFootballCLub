a7f343235f0bc1a75e75990617956f550cc1a856
regan-ku <ronaldregan609@gmail.com>
2026-09-23 13:32:55 +0300

    ercel deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 
 // Layout Components
 import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer"; 
+import Footer from "@/components/layout/footer"; 
 
 // UI Enhancements
 import CookieBanner from "@/components/ui/CookieBanner";
